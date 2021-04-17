271b841e61f60b7483962a1c18dfa74b2c71ebb5
Gledson Duarte <duarte.gledson@gmail.com>
2021-04-17 08:52:09 -0300

    Implemented basic implementation of Snack machine class

---
+++ Domain/SnackMachine/SnackMachine.cs
@@ -0,0 +1,24 @@
+namespace Domain.SnackMachine
+{
+    public sealed class SnackMachine
+    {
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransaction { get; private set; }
+
+        public void InsertMoney(Money money)
+        {
+            MoneyInTransaction += money;
+        }
+
+        public void ReturnMoney()
+        {
+            // MoneyInTransaction = 0;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransaction;
+            // MoneyInTransaction = 0;
+        }
+    }
+}

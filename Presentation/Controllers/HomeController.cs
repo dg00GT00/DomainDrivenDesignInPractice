c50d7b3deba080aeb85c176ff6c98cf1f7958362
Gledson Duarte <duarte.gledson@gmail.com>
2021-05-04 13:45:23 -0300

    Added action to home controller

--- Presentation/Controllers/HomeController.cs
+++ Presentation/Controllers/HomeController.cs
@@ -5,20 +5,79 @@ using Presentation.Models;
 
 namespace Presentation.Controllers
 {
+    [ValidateAntiForgeryToken]
     public class HomeController : Controller
     {
-        public SnackMachine SnackMachine { get; set; }
+        private readonly SnackMachine _snackMachine;
 
         public HomeController(SnackMachine snackMachine)
         {
-            SnackMachine = snackMachine;
+            _snackMachine = snackMachine;
         }
 
+        [IgnoreAntiforgeryToken]
         public IActionResult Index()
         {
-            return View();
+            return View(new SnackModel
+            {
+                Machine = _snackMachine,
+                Message = "You have inserted: None"
+            });
         }
 
+        [HttpPost]
+        public IActionResult Insert(decimal value)
+        {
+            Money money = Money.None;
+            if (value == Money.Cent.Amount)
+            {
+                money = Money.Cent;
+            }
+
+            if (value == Money.Quarter.Amount)
+            {
+                money = Money.Quarter;
+            }
+
+            if (value == Money.TenCent.Amount)
+            {
+                money = Money.TenCent;
+            }
+
+            if (value == Money.Dollar.Amount)
+            {
+                money = Money.Dollar;
+            }
+
+            if (value == Money.FiveDollar.Amount)
+            {
+                money = Money.FiveDollar;
+            }
+
+            if (value == Money.TwentyDollar.Amount)
+            {
+                money = Money.TwentyDollar;
+            }
+
+            _snackMachine.InsertMoney(money);
+            return View("Index", new SnackModel {Machine = _snackMachine, Message = $"You have inserted: {money}"});
+        }
+
+        [HttpPost]
+        public IActionResult ReturnMoney()
+        {
+            _snackMachine.ReturnMoney();
+            return View("Index", new SnackModel {Machine = _snackMachine, Message = "The money was returned"});
+        }
+
+        [HttpPost]
+        public IActionResult Buy()
+        {
+            _snackMachine.BuySnack();
+            return View("Index",
+                new SnackModel
+                    {Machine = _snackMachine, Message = "Congratulations you have just brought a delicious snack!"});
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
